fe1e422539f136644d0440606554a1bdb6ba8731
Razin Syakib <muhammadrazin14@gmail.com>
2023-05-17 03:55:34 +0700

    Added waifus data to waifu.js and waifu.json
    Included an array of waifus data in the waifu.js and waifu.json file. The 'waifus' array contains information about characters such as Mitsuha Miyamizu and Kato Megumi, including their names, associated anime, and ages.

--- routes/waifus.js
+++ routes/waifus.js
@@ -2,8 +2,22 @@ import express from "express";
 
 const router = express.Router();
 
+const waifus = [
+  {
+    name: "Mitsuha Miyamizu",
+    anime: "Kimi no Nawa",
+    age: 17,
+  },
+  {
+    name: "Kato Megumi",
+    anime: "Saenai Heroine no Sodatekata",
+    age: 17,
+  },
+];
+
 router.get("/", (req, res) => {
-  res.send("All of cute waifu");
+  console.log(waifus);
+  res.send(waifus);
 });
 
 export default router;
