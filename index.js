b86856e21d9134daa87037a173889455d3a4aad5
Razin Syakib <muhammadrazin14@gmail.com>
2023-05-17 03:10:48 +0700

    Implemented GET route for waifus endpoint
    Added a GET route handler in waifus.js to handle the retrieval of waifu data from the /waifus endpoint. Implemented the logic to fetch and return the waifu data when a GET request is made to the /waifus endpoint.

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+import waifuRoutes from "./routes/waifus.js";
+
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
+
+app.use("/waifus", waifuRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello from Homepage");
