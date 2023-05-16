bdf3566054db2a9537714549936f4ed1db266b67
Razin Syakib <muhammadrazin14@gmail.com>
2023-05-17 01:58:56 +0700

    Basic server setup using Express with PORT 5000

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 import express from "express";
+import bodyParser from "bodyParser";
+
+const app = express();
+const PORT = 5000;
+
+app.use(bodyParser.json());
+
+app.listen(PORT, () =>
+  console.log(`Server running on port: http://localhost${PORT}`)
+);
