9804881da3477ea73ab2619f8b2dfe4f905f4341
Razin Syakib <muhammadrazin14@gmail.com>
2023-05-17 02:43:05 +0700

    Implemented GET route for root endpoint
    Created a route handler for the root endpoint ('/') to handle GET requests. Added responded with the message 'Hello from Homepage'

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const PORT = 5000;
 
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello from Homepage");
+});
+
 app.listen(PORT, () =>
   console.log(`Server running on port: http://localhost:${PORT}`)
 );
