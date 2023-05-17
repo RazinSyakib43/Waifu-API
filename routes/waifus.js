ece117b3730b0078a33f8b353450aeae6658155b
Razin Syakib <muhammadrazin14@gmail.com>
2023-05-17 10:03:35 +0700

    Implemented POST and GET routes for waifus endpoint
    Added the POST route to handle requests for adding a new waifu to the waifus array. The waifu object received from the request body is assigned a unique ID generated using the uuid module. The waifu is then pushed into the waifus array. Also implemented the GET route to retrieve a waifu based on its ID. The route searches for the waifu with the matching ID in the waifus array and sends it as the response

--- routes/waifus.js
+++ routes/waifus.js
@@ -1,19 +1,8 @@
 import express from "express";
-
+import { v4 as uuidv4 } from 'uuid';
 const router = express.Router();
 
-const waifus = [
-  {
-    name: "Mitsuha Miyamizu",
-    anime: "Kimi no Nawa",
-    age: 17,
-  },
-  {
-    name: "Kato Megumi",
-    anime: "Saenai Heroine no Sodatekata",
-    age: 17,
-  },
-];
+const waifus = [];
 
 router.get("/", (req, res) => {
   console.log(waifus);
@@ -22,9 +11,20 @@ router.get("/", (req, res) => {
 
 router.post("/", (req, res) => {
   const waifu = req.body;
-  waifus.push(waifu);
+
+  const waifuWithId = { ...waifu, id:uuidv4()};
+
+  waifus.push(waifuWithId);
 
   res.send(`Your waifu, ${waifu.name}, has been added to the list!`);
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  const foundWaifu = waifus.find((waifu) => waifu.id === id);
+
+  res.send(foundWaifu);
 });
 
 export default router;
