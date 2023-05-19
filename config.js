15d6de7d42a36f73bb4bdbbea7fd4a028b4c49a2
Razin Syakib <muhammadrazin14@gmail.com>
2023-05-19 11:50:00 +0700

    Update Firebase and Firestore Configuration
    Updated the code to use the CommonJS module syntax for Firebase import and corrected the initialization method. Reconfigured the Firebase and Firestore settings with the correct credentials and identifiers. Exported the "waifus" collection reference from the Firestore database.

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
-firebaseConfig = {
+const firebase = require('firebase');
+const firebaseConfig = {
     apiKey: "AIzaSyAtzHZ1F_UGGZJvvum4BpRUxcPV7p3-7fs",
     authDomain: "waifu-api-dev.firebaseapp.com",
     projectId: "waifu-api-dev",
@@ -7,3 +8,8 @@ firebaseConfig = {
     appId: "1:483831202881:web:0352a403a1f0fddedef2c0",
     measurementId: "G-XWJB90XLVP"
   };
+
+  firebase.initializeAoo(firebaseConfig);
+  const db=firebase.firestore();
+  const waifu=db.collection("waifus");
+  module.exports = waifus;
