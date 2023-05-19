7ee9db58ad9f9a205ed987bbee57636e005e6f75
Razin Syakib <muhammadrazin14@gmail.com>
2023-05-19 11:41:30 +0700

    Add Firebase configuration
    Added Firebase configuration to connect the application with the associated Firebase project.

---
+++ config.js
@@ -0,0 +1,9 @@
+firebaseConfig = {
+    apiKey: "AIzaSyAtzHZ1F_UGGZJvvum4BpRUxcPV7p3-7fs",
+    authDomain: "waifu-api-dev.firebaseapp.com",
+    projectId: "waifu-api-dev",
+    storageBucket: "waifu-api-dev.appspot.com",
+    messagingSenderId: "483831202881",
+    appId: "1:483831202881:web:0352a403a1f0fddedef2c0",
+    measurementId: "G-XWJB90XLVP"
+  };
